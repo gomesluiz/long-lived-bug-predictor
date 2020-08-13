262f829955c92e54ea166c2a212c4216d3576563
Luiz Alberto <gomes.luiz@gmail.com>
2020-08-13 09:45:01 -0300

    README updated

--- r3_1a_predict_long_lived_bug.R
+++ r3_1a_predict_long_lived_bug.R
@@ -22,7 +22,7 @@ timestamp <- format(Sys.time(), "%Y%m%d")
 # Constants --------------------------------------------------------------------
 debug_on     <- FALSE
 force_create <- TRUE
-processors   <- ifelse(debug_on, 3, 3)
+processors   <- ifelse(debug_on, 3, 7)
 base_path   <- file.path("~", "Workspace", "long-lived-bug-predictor-ml-in-r")
 lib_path    <- file.path(base_path, "R")
 data_path   <- file.path(base_path, "data")
