9dc652a99d6895ce491d9e97670217e89d29dd3a
Luiz Alberto <gomes.luiz@gmail.com>
2020-08-13 11:11:25 -0300

    README updated

--- scripts/run_1.sh
+++ scripts/run_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IMAGE=long-lived-bug-prediction-ml-v1
-SHAREDIR=/home/lgomes/Workspace/long-lived-bug-predictor-ml-in-r/data/
-WORKDIR=/home/lgomes/Workspace/long-lived-bug-predictor-ml-in-r/
-LOGFILE=/home/lgomes/Workspace/long-lived-bug-predictor-ml-in-r/output/logs/r3_1a_predict_long_lived_bug.error
+SHAREDIR=/home/g348286/Workspace/long-lived-bug-predictor-ml-in-r/data/
+WORKDIR=/home/g348286/Workspace/long-lived-bug-predictor-ml-in-r/
+LOGFILE=/home/g348286/Workspace/long-lived-bug-predictor-ml-in-r/output/logs/r3_1a_predict_long_lived_bug.error
 docker run -d -it --ipc=host --userns=host -v ${SHAREDIR}/:${SHAREDIR}/ --workdir ${WORKDIR}/ ${IMAGE}:latest Rscript ${WORKDIR}/r3_1a_predict_long_lived_bug.R | tee -a $LOGFILE
