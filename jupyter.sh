ece8615cf13be1a679b5fe73a866777e2ff79e45
Luiz Alberto <gomes.luiz@gmail.com>
2020-08-15 14:46:19 -0300

    README updated

---
+++ jupyter.sh
@@ -0,0 +1,2 @@
+source ./env/bin/activate
+nohup jupyter notebook > /dev/null 2>&1 &
--- scripts/run_2.sh
+++ scripts/run_2.sh
@@ -3,5 +3,5 @@
 IMAGE=long-lived-bug-prediction-ml-v1
 SHAREDIR=/home/lgomes/Workspace/long-lived-bug-predictor-ml-in-r/
 WORKDIR=/home/lgomes/Workspace/long-lived-bug-predictor-ml-in-r/
-LOGFILE=/home/lgomes/Workspace/long-lived-bug-predictor-ml-in-r/output/logs/r3_1b_predict_long_lived_bug.error
-docker run -d -it --ipc=host --userns=host -v ${SHAREDIR}/:${SHAREDIR}/ --workdir ${WORKDIR}/ ${IMAGE}:latest Rscript ${WORKDIR}/r3_1b_predict_long_lived_bug.R | tee -a $LOGFILE
+LOGFILE=/home/lgomes/Workspace/long-lived-bug-predictor-ml-in-r/output/logs/r3_2b_predict_long_lived_bug.error
+docker run -d -it --ipc=host --userns=host -v ${SHAREDIR}/:${SHAREDIR}/ --workdir ${WORKDIR}/ ${IMAGE}:latest Rscript ${WORKDIR}/r3_2b_predict_long_lived_bug.R | tee -a $LOGFILE
