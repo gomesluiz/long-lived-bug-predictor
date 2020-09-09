e3d785c8982e06c6215e783135625c57def88b07
Luiz Alberto <gomes.luiz@gmail.com>
2020-09-08 23:08:32 -0300

    contents updated

---
+++ scripts/run_4_3a.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+USER=`whoami`
+IMAGE=long-lived-bug-prediction-ml-v1
+SHAREDIR=/home/${USER}/Workspace/long-lived-bug-predictor-ml-in-r/
+WORKDIR=/home/${USER}/Workspace/long-lived-bug-predictor-ml-in-r/
+LOGFILE=/home/${USER}/Workspace/long-lived-bug-predictor-ml-in-r/output/logs/r4_3a_predict_long_lived_bug.error
+docker run -d -it --ipc=host --userns=host -v ${SHAREDIR}/:${SHAREDIR}/ --workdir ${WORKDIR}/ ${IMAGE}:latest Rscript ${WORKDIR}/r4_3a_predict_long_lived_bug.R | tee -a $LOGFILE
