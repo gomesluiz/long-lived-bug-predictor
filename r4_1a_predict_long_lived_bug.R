c7ab795f544ffc3ee083d468a521b2fac26b799b
Luiz Alberto <gomes.luiz@gmail.com>
2020-09-11 17:08:18 +0200

    contents updated

--- r4_1a_predict_long_lived_bug.R
+++ r4_1a_predict_long_lived_bug.R
@@ -20,7 +20,7 @@ options(readr.num_columns = 0)
 timestamp <- format(Sys.time(), "%Y%m%d")
 
 # Constants --------------------------------------------------------------------
-debug_on     <- TRUE
+debug_on     <- FALSE
 force_create <- TRUE
 processors   <- ifelse(debug_on, 3, 30)
 base_path   <- file.path("~", "Workspace", "long-lived-bug-predictor-ml-in-r")
@@ -102,7 +102,7 @@ if (debug_on) {
     appender.file(
       file.path(
         output_path, "logs",
-        sprintf("%s_r3_1a_predict_long_lived_bug.log", timestamp)
+        sprintf("%s_r4_1a_predict_long_lived_bug.log", timestamp)
       )
     )
   )
